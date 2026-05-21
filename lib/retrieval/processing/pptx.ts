4eee8ad9dc6d05c86784f76e7bafc2f74d7744e5
jmlee26 <wjdals5267@gmail.com>
2026-05-21 14:06:49 +0900

    Update pptx.ts

--- lib/retrieval/processing/pptx.ts
+++ lib/retrieval/processing/pptx.ts
@@ -1,5 +1,5 @@
 import JSZip from "jszip"
-import { xml2js } from "xml-js"
+import * as xmljs from "xml-js"
 
 export async function processPPTX(blob: Blob) {
   const arrayBuffer = await blob.arrayBuffer()
@@ -15,17 +15,33 @@ export async function processPPTX(blob: Blob) {
   for (const slidePath of slideFiles) {
     const slideXml = await zip.files[slidePath].async("text")
 
-    const matches = slideXml.match(/<a:t>(.*?)<\/a:t>/g)
+    const parsed = xmljs.xml2js(slideXml, {
+      compact: true
+    }) as any
 
-    if (matches) {
-      const texts = matches.map(text =>
-        text
-          .replace("<a:t>", "")
-          .replace("</a:t>", "")
-      )
+    const texts: string[] = []
 
-      fullText += texts.join(" ") + "\n"
+    const extractText = (obj: any) => {
+      if (!obj || typeof obj !== "object") return
+
+      for (const key in obj) {
+        if (key === "a:t") {
+          if (Array.isArray(obj[key])) {
+            obj[key].forEach((t: any) => {
+              if (t._text) texts.push(t._text)
+            })
+          } else if (obj[key]._text) {
+            texts.push(obj[key]._text)
+          }
+        }
+
+        extractText(obj[key])
+      }
     }
+
+    extractText(parsed)
+
+    fullText += texts.join(" ") + "\n"
   }
 
   return [
