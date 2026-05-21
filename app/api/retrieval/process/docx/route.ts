748d4369a1b319675c64f047d40e370e2e0896ad
jmlee26 <wjdals5267@gmail.com>
2026-05-21 12:24:39 +0900

    Update route.ts

--- app/api/retrieval/process/docx/route.ts
+++ app/api/retrieval/process/docx/route.ts
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
         })
     }
 
-    let embeddings: any = []
+    //let embeddings: any = []
 
     //let openai
     //if (profile.use_azure_openai) {
