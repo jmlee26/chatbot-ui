09d08465aaae73922dc3b3926bf8a95b0d8b09b0
jmlee26 <wjdals5267@gmail.com>
2026-05-21 12:42:40 +0900

    Update use-select-file-handler.tsx

--- components/chat/chat-hooks/use-select-file-handler.tsx
+++ components/chat/chat-hooks/use-select-file-handler.tsx
@@ -8,11 +8,11 @@ import { toast } from "sonner"
 export const ACCEPTED_FILE_TYPES = [
   "text/csv",
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-
   "application/vnd.ms-excel",
-  
+
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+
   "application/json",
   "text/markdown",
   "application/pdf",
